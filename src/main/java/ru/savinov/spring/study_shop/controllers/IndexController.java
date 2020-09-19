98c688c0b558db1d98cefcfc86090d752e0aeb6d
Naglezh <savinov_vu@mail.ru>
2020-09-19 17:19:13 +0800

    Добавлен тест IndexControllerTest без поднятия спринг контекста.

--- src/main/java/ru/savinov/spring/study_shop/controllers/IndexController.java
+++ src/main/java/ru/savinov/spring/study_shop/controllers/IndexController.java
@@ -2,11 +2,13 @@ package ru.savinov.spring.study_shop.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
     @GetMapping("/index")
-    public String homePage() {
-        return "index";
+    public ModelAndView homePage() {
+        ModelAndView mav = new ModelAndView("index.html");
+        return mav;
     }
 }
