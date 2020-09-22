5df5612205a8bf330af0f39579bb3b61560e2bc7
Naglezh <savinov_vu@mail.ru>
2020-09-22 21:26:13 +0800

    ProductServiceTest update: add testGetAllProduct.

--- src/test/java/ru/savinov/spring/shop/services/ProductServiceTest.java
+++ src/test/java/ru/savinov/spring/shop/services/ProductServiceTest.java
@@ -10,7 +10,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ru.savinov.spring.shop.entities.Product;
 import ru.savinov.spring.shop.repositories.ProductRepository;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
 
@@ -21,12 +22,32 @@ class ProductServiceTest {
     ProductRepository productRepository;
     ProductService productService;
     Product testProduct;
+    List<Product> testListProducts;
 
     @BeforeEach
     public void init() {
         productService = new ProductService(productRepository);
         testProduct = new Product(1L, "Chiken", 99);
 
+        testListProducts = new ArrayList<>();
+        testListProducts.add(testProduct);
+        testListProducts.add(new Product(2L, "Milk", 88));
+        testListProducts.add(new Product(3L, "Fish", 77));
+    }
+
+    @Test
+    void testGetAllProduct() {
+        List<Product> falseList = new ArrayList<>();
+        falseList.addAll(testListProducts);
+        falseList.set(2, new Product(3L, "Finish", 77));
+        System.out.println(falseList);
+
+        when(productRepository.findAll()).thenReturn(testListProducts);
+
+        List<Product> acceptList = productService.getAllProducts();
+
+        Assert.assertNotEquals(falseList, acceptList);
+        Assert.assertEquals(testListProducts, acceptList);
     }
 
     @Test
