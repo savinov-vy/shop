6d325d635b49b6e165ee6407aa9d19f9336ea7f7
savinov85 <savinov_vu@mail.ru>
2020-10-29 23:35:49 +0800

    проброшена сущность backend -> frontend

--- src/main/java/ru/savinov/spring/shop/controllers/ProductControllerA.java
+++ src/main/java/ru/savinov/spring/shop/controllers/ProductControllerA.java
@@ -1,10 +1,8 @@
 package ru.savinov.spring.shop.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.savinov.spring.shop.entities.Product;
-import ru.savinov.spring.shop.entities.User;
 import ru.savinov.spring.shop.services.ProductService;
 
 @RestController
@@ -12,11 +10,14 @@ public class ProductControllerA {
     @Autowired
     private ProductService productService;
 
-@RequestMapping(method = RequestMethod.GET, value = "/a/get_product")
+    @GetMapping("/a1")
     public Product getTestProduct() {
-
-
-    return productService.getProductById(1L);
+    return (productService.getProductById(1L));
 }
+
+    @PostMapping("/add")
+    public void addTest(@RequestBody Product product) {
+        System.out.println(product);
+        }
 
 }
---
+++ src/main/java/ru/savinov/spring/shop/utils/Converter.java
@@ -0,0 +1,13 @@
+package ru.savinov.spring.shop.utils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ru.savinov.spring.shop.entities.User;
+
+import java.io.IOException;
+
+public class Converter {
+
+    public static User toJavaObject(String strJSON) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(strJSON, User.class);
+    }
+}
