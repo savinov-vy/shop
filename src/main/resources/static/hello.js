68dccc87fb6913031a0efdec67003b77baac4cd9
savinov85 <savinov_vu@mail.ru>
2020-10-29 20:40:59 +0800

    Рабочая форма обратной связи

---
+++ src/main/resources/static/hello.js
@@ -0,0 +1,24 @@
+
+$("#sendMail").bind("click", function () {
+    alert("hello");
+/*    var email = $("#email").val().trim();
+    var name = $("#name").val().trim();
+    var phone = $("#phone").val().trim();
+    var message = $("#message").val().trim();
+
+    if (email == "") {
+
+        $("#errorMess").text("Введите email");
+        return false;
+    } else if (name == "") {
+        $("#errorMess").text("Введите имя");
+        return false;
+    } else if (phone == "") {
+        $("#errorMess").text("Введите телефон");
+        return false;
+    } else if (message.length<5) {
+        $("#errorMess").text("Введите сообщение не менее 5 символов");
+        return false;
+    }
+    $("#errorMess").text("");*/
+});
