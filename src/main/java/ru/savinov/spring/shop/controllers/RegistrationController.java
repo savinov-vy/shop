c04be8cda4e62d5a35748cd54ca97a76ab743fbf
savinov85 <savinov_vu@mail.ru>
2020-11-24 23:17:00 +0800

    add UserValidator: добавлена функция проверки user name в базе данных при регистрации пользователя

--- src/main/java/ru/savinov/spring/shop/controllers/RegistrationController.java
+++ src/main/java/ru/savinov/spring/shop/controllers/RegistrationController.java
@@ -3,15 +3,21 @@ package ru.savinov.spring.shop.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.savinov.spring.shop.entities.User;
 import ru.savinov.spring.shop.services.UserService;
+import ru.savinov.spring.shop.services.UserValidator;
+
 import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
 
     private UserService userService;
+
+    @Autowired
+    private UserValidator userValidator;
 
     @Autowired
     public RegistrationController(UserService userService) {
@@ -25,7 +31,12 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration/form")
-    public String addUser(@ModelAttribute("user") @Valid User userForm) {
+    public String addUser(@ModelAttribute("user") @Valid User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if(bindingResult.hasErrors()){
+            return "reg";
+        }
 
         userService.save(userForm);
 
