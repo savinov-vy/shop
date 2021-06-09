18b604afefedcc21b77fb985212c9e2f6ed0f8ea
savinov <savinov_vu@mail.ru>
2021-06-09 23:42:23 +0800

    Aspect @After с использованием joinPoint

--- src/main/java/ru/savinov/spring/shop/utils/aspects/LoggingAspect.java
+++ src/main/java/ru/savinov/spring/shop/utils/aspects/LoggingAspect.java
@@ -1,7 +1,13 @@
 package ru.savinov.spring.shop.utils.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import ru.savinov.spring.shop.entities.Role;
+import ru.savinov.spring.shop.entities.User;
+
+import java.util.Set;
 
 @Component
 @Aspect
@@ -15,5 +21,33 @@ public class LoggingAspect {
     @After("execution(public void createNewUser(ru.savinov.spring.shop.entities.User))")
     public void afterCreateNewUserAdvice() {
         System.out.println("afterCreateNewUserAdvice: создан новый пользователь");
+    }
+
+    @After("execution(public void save(..))")
+    public void afterSaveAdvice(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("afterSaveAdvice method: " + methodSignature.getMethod());
+        System.out.println("afterSaveAdvice return type: " + methodSignature.getReturnType());
+        System.out.println("afterSaveAdvice method name: " + methodSignature.getName());
+
+        System.out.println("************************************************************");
+
+        Object [] arguments = joinPoint.getArgs();
+        for (Object obj : arguments) {
+            if (obj instanceof User) {
+                User user = (User) obj;
+                System.out.println("User name: " + user.getUsername());
+                System.out.println("User id: " + user.getId());
+                System.out.println("User password: " + user.getPassword());
+                System.out.println("User enabled " + user.getEnabled());
+                Set<Role> roles = user.getRoles();
+
+                for (Role role : roles) {
+                    System.out.println("role name: " + role.getName());
+                    System.out.println("role id: " + role.getId());
+
+                }
+            }
+        }
     }
 }
