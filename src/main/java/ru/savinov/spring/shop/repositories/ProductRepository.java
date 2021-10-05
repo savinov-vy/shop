aa05564caf8d44c3333006e86ae84d034b4660e8
savinov <savinov_vu@mail.ru>
2021-10-05 12:36:30 +0800

    add: README.md

--- src/main/java/ru/savinov/spring/shop/repositories/ProductRepository.java
+++ src/main/java/ru/savinov/spring/shop/repositories/ProductRepository.java
@@ -1,15 +1,11 @@
 package ru.savinov.spring.shop.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.savinov.spring.shop.entities.Product;
-
-import java.util.List;
-
 
 @Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long>, JpaSpecificationExecutor<Product> {
