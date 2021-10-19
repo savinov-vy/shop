aa85a5a02b0e7b5603ee25399a5bba155474e08c
savinov <savinov_vu@mail.ru>
2021-10-19 23:21:19 +0800

    prepare integration test ShopController

---
+++ src/test/java/ru/savinov/shop/controllers/ShopControllerTest.java
@@ -0,0 +1,14 @@
+package ru.savinov.shop.controllers;
+
+import org.junit.jupiter.api.Test;
+import ru.savinov.shop.AbstractTest;
+
+
+public class ShopControllerTest extends AbstractTest {
+
+
+    @Test
+    public void testShopPage() throws Exception {
+
+    }
+}
