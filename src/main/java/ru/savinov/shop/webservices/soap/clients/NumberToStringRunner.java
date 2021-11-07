79fd9199f1c1ac68cb7843be244b2b4139252c89
savinov <savinov_vu@mail.ru>
2021-11-07 14:09:19 +0800

    create simple soap client: number converter

---
+++ src/main/java/ru/savinov/shop/webservices/soap/clients/NumberToStringRunner.java
@@ -0,0 +1,26 @@
+package ru.savinov.shop.webservices.soap.clients;
+
+import ru.savinov.shop.webservices.soap.clients.numberToString.NumberConversion;
+import ru.savinov.shop.webservices.soap.clients.numberToString.NumberConversionSoapType;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+public class NumberToStringRunner {
+    public static void main(String[] args) throws MalformedURLException {
+
+        final String endpoint = "https://www.dataaccess.com/webservicesserver/numberconversion.wso";
+
+        final URL url = URI.create(endpoint).toURL();
+
+        final NumberConversion service = new NumberConversion(url);
+
+        final NumberConversionSoapType port = service.getPort(NumberConversionSoapType.class);
+
+        System.out.println("$15.99 --> " + port.numberToDollars(BigDecimal.valueOf(15.99)));
+        System.out.println("34  --> " + port.numberToWords(BigInteger.valueOf(34)));
+    }
+}
---
+++ src/main/java/ru/savinov/shop/webservices/soap/clients/numberToString/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.dataaccess.com/webservicesserver/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package ru.savinov.shop.webservices.soap.clients.numberToString;
