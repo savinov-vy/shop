d19c60c1d6b077e3306e2fe475a2b6f6910400b7
savinov <savinov_vu@mail.ru>
2022-02-25 12:40:14 +0800

    tmp

--- src/main/java/ru/savinov/shop/common/ConstantProperties.java
+++ src/main/java/ru/savinov/shop/common/ConstantProperties.java
@@ -5,7 +5,5 @@ public class ConstantProperties {
     public static final int PAGE_SIZE = 10;
     public static final int INITIAL_PAGE = 0;
     public static final String ANONYMOUS_USER = "anonymousUser";
-    public static final String STATUS_OK = "OK";
-
 
 }
